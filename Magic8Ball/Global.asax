2af0659710595b2e87d2c0b9ef8acfb7fb85f839
Sam Jacobson <samuel.c.jacobson@gmail.com>
2017-08-08 15:42:44 -0400

    first commit

---
+++ Magic8Ball/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Magic8Ball.MvcApplication" Language="C#" %>
---
+++ Magic8Ball/Scripts/8ball.js
@@ -0,0 +1,19 @@
+﻿function wait(ms)
+{
+    var d = new Date();
+    var d2 = null;
+    do { d2 = new Date(); }
+    while(d2-d < ms);
+}
+
+function Fortune() {
+
+
+    wait(2000);
+    var fortunes = ["It is certain", "Don't count on it", "It is decidedly so", "Better not tell you now", "My sources say no"];
+    var randFortune = Math.round(Math.random() * (fortunes.length-1));
+    document.getElementById("fortuneText").innerHTML = fortunes[randFortune];
+
+    wait(5000);
+    document.getElementById("fortuneText").visibility = "hidden";
+}
